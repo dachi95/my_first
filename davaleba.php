06e61cd7cc9b7ca7569f502978ab6643edc5476d
Dachi <dachi@Omedia-Staging.(none)>
2014-03-11 21:24:58 +0000

    ideashi unda mushaobdes

--- davaleba.php
+++ davaleba.php
@@ -7,5 +7,10 @@ curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
 $fullcode = curl_exec($ch);
 curl_close($ch);
 preg_match ('/(<table class="league-table">.+?<\/table> ?)+/',$fullcode, $scores);
-print "<center> $scores[0] </center>";
+
+preg_match ('/<td class="fh">[a-zA-Z0-9]+<\/td> ?/',$fullcode, $fclub);
+preg_match ('/<td class="fa">[a-zA-Z0-9]+<\/td> ?/',$fullcode, $sclub);
+
+//print "<center> $scores[0] </center>";
+print_r ("$fclub . $sclub");
 ?>
