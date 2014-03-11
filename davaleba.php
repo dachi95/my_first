9c234eddbe182479f229dfc0222b53cbeefa57e1
Dachi <dachi@Omedia-Staging.(none)>
2014-03-11 20:07:33 +0000

    viwyeb davalebaze mushaobas

--- davaleba.php
+++ davaleba.php
@@ -1,16 +1,11 @@
 <?php
-  $ch=curl_init();
-  curl_setopt($ch, CURLOPT_URL , 'http://WWW.livescore.com' );
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER , TRUE);
-  curl_setopt($ch, CURLOPT_FOLLOWLOCATION , TRUE);
-  $table=''
-  $output = curl_exec($ch);
-preg_match( $output, $table );
+$ch=curl_init();
 
-print $output;
-
+curl_setopt($ch, CURLOPT_URL, "http://livescore.com/");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE); 
+$fullcode = curl_exec($ch);
 curl_close($ch);
-
-
-
+preg_match ('/(<table class="league-table">.+?<\/table> ?)+/',$fullcode, $scores);
+print "<center> $scores[0] </center>";
 ?>
